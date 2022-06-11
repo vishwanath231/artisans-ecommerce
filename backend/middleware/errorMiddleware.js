38f163518a353d831c9dc1408b0a2e41f5489b40
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-06-11 19:39:44 +0530

    Error middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,20 @@
+const errorHandler = (err, req, res, next) => {
+
+    const stCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    res.status(stCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+}
+
+
+const notFound = (req, res, next) => {
+
+    const error = new Error(`Not Found ${req.originalUrl}`)
+    req.status(404)
+    next(error)
+}
+
+export { errorHandler, notFound };
