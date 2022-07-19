22f79a84363accbb1e655dd980c2ed3c8b80f95e
venkat <venghada@gmail.com>
2022-07-19 22:15:16 +0530

    payment

--- frontend/src/screens/common/PaymentScreen.js
+++ frontend/src/screens/common/PaymentScreen.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SVGicon from '../../assets/svg/SVGicon'
-import CheckoutStep from '../../components/CheckoutStep'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import SVGicon from '../../assets/svg/SVGicon';
+import CheckoutStep from '../../components/CheckoutStep';
+import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-const PaymentScreen = () => {
+const PaymentScreen = ({ cart }) => {
+
+   const navigate = useNavigate()
+
+    const [paymentMethod, setPaymentMethod] = useState('PayPal')
+
+    const { shippingAddress } = cart;
+
+    if (!shippingAddress.address) {
+        navigate('/shipping')
+    }
+
+    const _submitHandler = e => {
+        e.preventDefault()
+
+        console.log(paymentMethod);
+    }
+
+
     return (
         <main className='min-screen-h'>
                 <div className='px-4 max-w-3xl my-9 mx-auto'>
@@ -14,9 +34,30 @@ const PaymentScreen = () => {
                     </div>
                     <CheckoutStep step1 step2 step3 />
                     <h2 className='text-4xl font-light uppercase mb-4'>Payment Method</h2> 
+                    
+                    <form onSubmit={_submitHandler} className='mt-10'>
+                        <div className="flex items-center mb-7">
+                            <input 
+                                id="PayPal" 
+                                type="radio" 
+                                value="PayPal" 
+                                name="paymentMethod"
+                                onChange={e => setPaymentMethod(e.target.value)} 
+                                className="w-4 h-4" 
+                                required 
+                            />
+                            <label htmlFor="PayPal" className="ml-2 text-base font-medium text-gray-900">PayPal or Credit Card</label>
+                        </div>
+                        <button type="submit" className="text-white text-xs tracking-wide bg-blue-700 uppercase hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  px-5 py-3.5 text-center mont-font">continue</button>
+                    </form>
                 </div>
+
             </main>
     )
 }
 
-export default PaymentScreen;
+const mapStateToProps = (state) => ({
+    cart: state.cart
+})
+
+export default connect(mapStateToProps, null)(PaymentScreen);
--- frontend/src/screens/common/ShippingScreen.js
+++ frontend/src/screens/common/ShippingScreen.js
@@ -109,7 +109,7 @@ const ShippingScreen = ({ saveShippingAddress, cart }) => {
                                 required  
                             />
                         </div>
-                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mont-font">submit</button>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs uppercase tracking-wide px-5 py-3.5 text-center mont-font">save and continue</button>
                     </form>
                 </div>
             </main>
